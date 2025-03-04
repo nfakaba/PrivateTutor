b8821279d3665ec7fbe6abbb9095c21833761084
nfakaba <cissefakaba@gmail.com>
2025-03-04 18:28:02 -0500

    Add files via upload
    Privat Tutor DB script

---
+++ PT.sql
@@ -0,0 +1,20 @@
+Use mydb;
+SELECT * FROM TUTOR;
+INSERT INTO Tutor 
+VALUES 
+(2,'Tamir', 'DeGreat', 'TDG@gmail.com', '210-395-2510', 15.00);
+
+INSERT INTO Subjects
+Values
+(10, 'English');
+
+INSERT INTO Lessons
+VALUES 
+(1);
+
+INSERT INTO STUDENTS
+VALUES
+(42, 'JORGE', 'LOPEZ', 'JLO@yahoo.com');
+SELECT * FROM TUTOR;
+
+
